9fa84fd03c2688926813756c2bfa182d0bc93f7e
Vladislav <vladislavsultan846@gmail.com>
2025-10-16 16:29:49 +0300

    base to future work. first step

---
+++ Polinomial/Polynomial.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinomial
+{
+    public struct Element
+    {
+        double Coefficient;
+        int Deegree;
+    }
+    internal class Polynomial : IPolynomial
+    {
+        private Element[] elements;
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+        Polynomial IPolynomial.Add(Polynomial secondPolynomial)
+        {
+            throw new NotImplementedException();
+        }
+
+        Polynomial IPolynomial.AddNumber(double num)
+        {
+            throw new NotImplementedException();
+        }
+
+        double IPolynomial.CalculateValue(double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        double IPolynomial.FindDerivative()
+        {
+            throw new NotImplementedException();
+        }
+
+        Polynomial IPolynomial.MultiplyByNumber(double num)
+        {
+            throw new NotImplementedException();
+        }
+
+        Polynomial IPolynomial.Subtraction(Polynomial secondPolynomial)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

d7793149b498f018b27ecb1f1246f4243f76b74e
Vladislav <vladislavsultan846@gmail.com>
2025-10-16 16:29:49 +0300

    base to future work. first step

---
+++ Polinomial/IPolynomial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinomial
+{
+    internal interface IPolynomial
+    {
+
+        Polynomial Add(Polynomial secondPolynomial);
+        Polynomial Subtraction(Polynomial secondPolynomial);
+        Polynomial MultiplyByNumber(double num);
+        Polynomial AddNumber(double num);
+        double CalculateValue(double value);
+        double FindDerivative();
+
+    }
+}
---
+++ PolynomialWorkTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PolynomialWorkTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PolynomialWorkTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1de089c3-3f00-47ee-a376-d54aa7b70f21")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
